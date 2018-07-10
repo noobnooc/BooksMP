91e26815d807e16322dc710614b19e1d1e892da4
Hardo <i@hardo.me>
2018-07-10 18:14:49 +0800

    完成用户界面

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,18 +1,24 @@
 // pages/user/user.js
+
+const app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    user: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    app.addOnRefreshListener(this.onRefresh);
+    this.setData({
+      user: app.globalData.user
+    })
   },
 
   /**
@@ -26,7 +32,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
@@ -40,14 +46,15 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+    app.removeOnRefreshListener(this.onRefresh);
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+    app.fetchUser()
+      .then(() => wx.stopPullDownRefresh());
   },
 
   /**
@@ -62,5 +69,15 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+
+  onRefresh() {
+    this.setData({ user: app.globalData.user });
+  },
+
+  switchUser() {
+    wx.navigateTo({
+      url: '/pages/init/init',
+    })
   }
 })
